b82f4af94ab87a82526b43f685f6dfac64b2d6d9
dongshaohan <234714973@qq.com>
2015-12-26 11:25:00 +0800

    0.1

--- src/store.js
+++ src/store.js
@@ -22,7 +22,76 @@
 
 })(this, function () {
 
-    return {
-
+    var _ = {
+        version: "0.1",
+        stringify: function (s) {
+            return d === undefined || typeof d === "function" ? d+'' : JSON.stringify(d);
+        },
+        parse: function (s) {
+            // if it doesn't parse, return as is
+            try { 
+                return JSON.parse(s); 
+            } catch(e) { 
+                return s; 
+            }
+        },
+        get: function (area, key) { 
+            return area.getItem(key); 
+        },
+        set: function (area, key, string) { 
+            area.setItem(key, string);
+        },
+        remove: function (area, key) {
+            area.removeItem(key); 
+        },
+        key: function (area, i) { 
+            return area.key(i); 
+        },
+        length: function (area) { 
+            return area.length; 
+        },
+        clear: function (area) { 
+            area.clear(); 
+        },
+        storeAPI: {
+            _area: localStorage,
+            each: function (fn, and) {
+                for ( var i = 0, m = _.length(this._area); i < m; i++ ) {
+                    var key = _.key(this._area, i);
+                    if ( key !== undefined ) {
+                        if ( fn.call(this, key, and || this.get(key)) === false ) {
+                            break;
+                        }
+                    }
+                    if ( m > _.length(this._area) ) { m--; i--; }
+                }
+                return and || this;
+            },
+            get: function (key) {
+                var s = _.get(this._area, key);
+                return s !== null ? _.parse(s) : s;
+            },
+            getAll: function () {
+                return this.each(function (k, all) { all[k] = this.get(k); }, {});
+            },
+            set: function (key, data) {
+                return _.set(this._area, key, _.stringify(data));
+            },
+            setAll: function (data) {
+                for ( var key in data ) {
+                    this.set(key, data[key]); 
+                }
+            },
+            remove: function (key) {
+                var d = this.get(key);
+                _.remove(this._area, key);
+                return d;
+            },
+            clear: function () {
+                _.clear(this._area);
+            }
+        }
     };
+
+    return _.storeAPI;
 });
